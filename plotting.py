41fc28efc1fc7fe9063f5c856f042a912cd7abdd
julgrahn <alexander.grahn@hotmail.se>
2020-07-07 04:26:15 +0200

    add hover tool to graph

--- plotting.py
+++ plotting.py
@@ -1,15 +1,18 @@
 from main import df
 from bokeh.plotting import figure, show, output_file
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, ColumnDataSource
+
+cds = ColumnDataSource(df)
 
 p = figure(x_axis_type = 'datetime', height = 100, width = 500, sizing_mode = 'scale_width', title = "Motion Graph")
 p.yaxis.minor_tick_line_color = None
 p.yaxis.ticker.desired_num_ticks = 1
 
-hover = HoverTool(tooltips = [("Start: ", "@Start"), ("End: ", "@End")])
+
+hover = HoverTool(tooltips = [("Start", "@Start"), ("End", "@End")])
 p.add_tools(hover)
 
-q = p.quad(left = df["Start"], right = df["End"], bottom = 0, top = 1, color = "purple")
+q = p.quad(left = "Start", right = "End", bottom = 0, top = 1, color = "purple", source = cds)
 
 output_file("Graph1.html")
 show(p)
